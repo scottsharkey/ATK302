8dfc6a994b344eeff8428ce0d4e4fb1df20989b0
Scott Sharkey <srshark@ilstu.edu>
2019-09-06 19:58:35 -0500

    egu jbhdu gbfd

--- p5/avatar/sketch.js
+++ p5/avatar/sketch.js
@@ -1,31 +1,43 @@
 function setup() {
   // put setup code here
   createCanvas(1600, 820);
-  background('navy');
+  //x = width / 2;
+  //y = height;
+  //background('navy');
   //var score = 0;
+    //noStroke();
+
+      //2EA54F
+      //35552E
+
+   background('navy');
+   fill('white');
+   textSize(24);
+   textFont('Georgia');
+   text("This sword is named Trench properly so for it was hidden deep within the bowls of the ocean.  Within it is housed an ancient one, an eldritch horror of great power from a place far beyond mortal comphrension.  The wielder of this weapon is given the abililty to harness some of its power.   Some of its abilities are to compell others to follow the wielders commands or inspire great fear into the hearts of their enemies.  The released state of the sword was its initial form the pressed state of the sword is its warped appearance altered by the ancient ones powerful magic.", 200, 200, 1200, height/2);
 }
 
   //var score = 0;
 
 function draw() {
+
   // put drawing code here
   //background('white');
   //fill('green');
-  fill ("grey");
-  rect(800, 400, 25, 440) ;
-  rect(775, 400, 25, 440) ;
-  rect(775, 50, 50, 400);
-  rect(600, 425, 400, 50) ;
-  ellipse(800, 400, 200, 200);
-  fill('orange');
-  ellipse(800, 400, 125, 125);
-  fill('red');
-  triangle(800, 350, 785, 400, 800, 450);
-  triangle(800, 350, 815, 400, 800, 450);
-  fill ("grey");
-  ellipse(600, 450, 75, 75);
-  ellipse(1000, 450, 75, 75);
-  ellipse(800, 50, 75, 75);
+
+  //stroke(50);
+ //fill(100);
+ //ellipse(x, y, 24, 24);
+
+ // Jiggling randomly on the horizontal axis
+ //x = x + random(-1, 1);
+ // Moving up at a constant speed
+ //y = y - 1;
+
+ // Reset to the bottom
+ //if (y < 0) {
+   //y = height;
+
   //fill('black');
   //text('Scott', width/2, height/2,);
   //fill('orange');
@@ -34,12 +46,140 @@ function draw() {
   //ellipse(400, 300, 150, 150)
   //ellipse(width/2, height/2, 150, 150)
   //text(score, 10, 10);
-}
 
-function mouseReleased(){
- //background('white');
+
+
 //background(random(255), random(255), random(255));
 //ellipse(mouseX, mouseY, 100, 100);
  //score ++
 
+}
+
+function mouseNotPressed(){
+  background('navy');
+  fill ("grey");
+  stroke('black');
+  rect(800, 400, 25, 440) ;
+  rect(775, 400, 25, 440) ;
+  rect(775, 50, 50, 400);
+  rect(600, 425, 400, 50) ;
+  ellipse(800, 400, 200, 200);
+
+  fill('orange');
+  ellipse(800, 400, 125, 125);
+
+  fill('yellow');
+  stroke('yellow');
+  ellipse(800,400, 100, 100)
+
+  fill('red');
+  stroke('red');
+  triangle(800, 350, 785, 400, 800, 450);
+  triangle(800, 350, 815, 400, 800, 450);
+
+  fill ("grey");
+  stroke('black');
+  ellipse(600, 450, 75, 75);
+  ellipse(1000, 450, 75, 75);
+  ellipse(800, 50, 75, 75);
+}
+
+function mousePressed(){
+  background(53, 85, 46);
+  fill(225, 181, 61);
+  rect(800, 400, 25, 440) ;
+  rect(775, 400, 25, 440) ;
+  rect(775, 50, 50, 400);
+
+  fill (46, 165, 79);
+  stroke('black');
+  rect(600, 425, 400, 50) ;
+  ellipse(800, 400, 200, 200);
+  ellipse(880, 370, 50, 50);
+  fill ('black');
+  ellipse(880, 370, 25, 25);
+  fill (46, 165, 79);
+  ellipse(810, 315, 40, 40);
+  fill ('black');
+  ellipse(810, 315, 20, 20);
+  fill (46, 165, 79);
+  ellipse(845, 325, 50, 50);
+  fill ('black');
+  ellipse(845, 325, 25, 25);
+  fill (46, 165, 79);
+  ellipse(880, 415, 40, 40);
+  fill ('black');
+  ellipse(880, 415, 20, 20);
+  fill (46, 165, 79);
+  ellipse(895, 395, 30, 30);
+  fill ('black');
+  ellipse(895, 395, 15, 15);
+  fill (46, 165, 79);
+  ellipse(875, 395, 25, 25);
+  fill ('black');
+  ellipse(875, 395, 10, 10);
+  fill (46, 165, 79);
+  ellipse(870, 340, 30, 30);
+  fill ('black');
+  ellipse(870, 340, 15, 15);
+  fill (46, 165, 79);
+  ellipse(855, 355, 20, 20);
+  fill ('black');
+  ellipse(855, 355, 10, 10);
+  fill (46, 165, 79);
+  ellipse(855, 355, 20, 20);
+  fill ('black');
+  ellipse(855, 355, 10, 10);
+
+  fill(141, 223, 43);
+  ellipse(800, 400, 125, 125);
+
+  fill('yellow');
+  stroke('yellow');
+  ellipse(800,400, 100, 100)
+
+  fill('green');
+  stroke('green');
+  triangle(800, 350, 785, 400, 800, 450);
+  triangle(800, 350, 815, 400, 800, 450);
+
+
+  fill (46, 165, 79);
+  stroke('black');
+  ellipse(600, 450, 75, 75);
+  ellipse(1000, 450, 75, 75);
+  ellipse(800, 50, 75, 75);
+
+
+}
+
+function mouseReleased(){
+  background('navy');
+  fill ("grey");
+  stroke('black');
+  rect(800, 400, 25, 440) ;
+  rect(775, 400, 25, 440) ;
+  rect(775, 50, 50, 400);
+  rect(600, 425, 400, 50) ;
+  ellipse(800, 400, 200, 200);
+
+
+  fill('orange');
+  ellipse(800, 400, 125, 125);
+
+  fill('yellow');
+  stroke('yellow');
+  ellipse(800,400, 100, 100)
+
+  fill('red');
+  stroke('red');
+  triangle(800, 350, 785, 400, 800, 450);
+  triangle(800, 350, 815, 400, 800, 450);
+
+
+  fill ("grey");
+  stroke('black');
+  ellipse(600, 450, 75, 75);
+  ellipse(1000, 450, 75, 75);
+  ellipse(800, 50, 75, 75);
 }
