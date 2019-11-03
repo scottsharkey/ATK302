58bd3b450c27a985127bfa746c508f41fc329043
scottsharkey <54325551+scottsharkey@users.noreply.github.com>
2019-11-03 16:18:40 -0600

    weather

---
+++ p5/Week11/Week11Weather/sketch.js
@@ -0,0 +1,108 @@
+/* For mobile phones - accesses accelerometer.
+Make sure you turn on orientation lock on your iPhone or Android device. */
+
+var weather;
+var temp = 0;
+var weatherID = 0; // returned in the JSON weather element
+var myState = 0;
+
+
+function setup() {
+  createCanvas(400, 400);
+
+  // HERE Is the call to get the weather. PLEASE PUT YOUR OWN ID IN HERE!!!
+  // MINE WILL STOP WORKING IF EVERYONE USES IT!!!
+
+  var myJSONString = 'https://api.openweathermap.org/data/2.5/weather?zip=61701,us&units=imperial&';
+  var myIDString = 'appid=2898099655be6a73267c3fefb0b1595f'; // Take out the x’s and ADD YOUR ID!!!
+  loadJSON(myJSONString + myIDString, gotData); // look - that gotData function happens when JSON comes back.
+
+}
+
+
+function gotData(data) {
+  weather = data;
+  console.log(data); // for debugging purposes, print out the JSON data when we get it.
+}
+
+
+function draw() {
+  switch (myState) {
+    case 0:
+      if (weather) {
+          myState = 1;
+      }
+      break;
+
+    case 1:
+    background(200) ;
+    text
+  // DECORATIONS
+  // Just a bunch of text commands to display data coming in from addEventListeners
+  textAlign(LEFT);
+  textSize(20);
+  fill('black');
+  text("orientation data:", 25, 25);
+  textSize(15);
+  text("alpha: " + alpha, 25, 50);
+  text("beta: " + beta, 25, 70);
+  text("gamma: " + gamma, 25, 90);
+  textSize(20);
+  text("acceleration data:", 25, 125);
+  textSize(15);
+  /*text("x = " + x.toFixed(2), 25, 150); // .toFixed means just show (x) decimal places
+  text("y = " + y.toFixed(2), 25, 170);
+  text("z = " + z.toFixed(4), 25, 190);*/
+
+  // MORE DECORATIONS - write that pretty ATK type on top.
+  fill('white');
+  noStroke();
+  textSize(300);
+  textAlign(CENTER);
+  text("atk", width / 2, height / 2);
+  text("we have weather", 20, 20) ;
+// parse the weather object and put some text for some at least 3 different weather data!
+      break;
+
+  }
+
+}
+
+// HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
+
+// Read in accelerometer data
+window.addEventListener('deviceorientation', function(e) {
+  alpha = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
+});
+
+
+// accelerometer Data
+window.addEventListener('devicemotion', function(e) {
+  // get accelerometer values
+  x = e.acceleration.x;
+  y = e.acceleration.y;
+  z = e.acceleration.z;
+});
+
+function Car() {
+  this.pos = createVector(100, 100);
+  this.vel = createVector(random(-5, 5), random(-5, 5));
+  this.r = random(255);
+  this.g = random(255);
+  this.b = random(255);
+
+  this.display = function() {
+    fill(this.r, this.g, this.b)
+    rect(this.pos.x, this.pos.y, 100, 50);
+  }
+
+  this.drive = function() {
+    this.pos.add(this.vel);
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+  }
+}
