83192fe03a5be20feee88fc4b41a4a9903f6ac1a
scottsharkey <54325551+scottsharkey@users.noreply.github.com>
2019-11-10 22:50:49 -0600

    boi

---
+++ p5/Week12/googledata/sketch.js
@@ -0,0 +1,80 @@
+var namesArray = [];
+var thumbsup;
+var thumbsdown;
+
+function setup() {
+
+  // Tabletop stuff, for getting google spreadsheet data in.
+  let url = '1w_cmxmvl1_DGsS1SvGqPTlZl5P2oqCUnN-47MIvuotk'; // this is KEY of the URL from the sheet
+  let settings = {
+    key: url, // The url of the published google sheet
+    callback: gotData, // A callback for when the data comes in
+    simpleSheet: true // This makes things simpler for just a single worksheet of rows
+  };
+
+  Tabletop.init(settings); // Grab the data from the spreadsheet!
+  // End Tabletop initialization stuff
+
+
+  // Regular setup code we usually have
+  createCanvas(600, 600);
+  textAlign(CENTER);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+
+}
+
+// The data comes back as an array of objects
+// Each object contains all the data for one row of the sheet
+function gotData(data) {
+
+  console.log(data); // Print the data in the console
+
+  // iterate through the array of data and create an object and push it on an array called namesArray
+  for (let i = 0; i < data.length; i++) {
+    namesArray.push(new Gnomes(data[i].Name, data[i].Gnomes)) ;
+   }
+
+}
+
+
+function draw() {
+  background('grey');
+
+  // // iterate through the namesArray and display the objects!
+  for (let i = 0 ; i < namesArray.length ; i++) {
+    namesArray[i].display() ;
+  }
+
+}
+
+
+// my circle class
+function Gnomes(myName, myShape) {
+  this.pos = createVector(random(width), random(height));
+  this.name = myName;
+  this.Gnomes = myShape;
+  this.pos = createVector(100, 100);
+  this.vel = createVector(random(-5, 5), random(-5, 5));
+
+
+  this.display = function() {
+    if (this.Gnomes == "Yes"){
+      ellipse(this.pos.x, this.pos.y, 100, 100);
+        this.pos.add(this.vel);
+        if (this.pos.x > width) this.pos.x = 0;
+        if (this.pos.x < 0) this.pos.x = width;
+        if (this.pos.y > height) this.pos.y = 0;
+        if (this.pos.y < 0) this.pos.y = height;
+        }
+    else{
+      rect(this.pos.x, this.pos.y,100, 100);
+        this.pos.add(this.vel);
+        if (this.pos.x > width) this.pos.x = 0;
+        if (this.pos.x < 0) this.pos.x = width;
+        if (this.pos.y > height) this.pos.y = 0;
+        if (this.pos.y < 0) this.pos.y = height;
+        }
+text(this.name, this.pos.x, this.pos.y);
+}
+}
